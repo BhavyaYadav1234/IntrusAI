86c1150d69eb498a69de22a3d9048becf05cafd7
Raunaksinha22 <raunaksinha1415@gmail.com>
2025-03-28 15:51:30 +0530

    changes again

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const idsRoutes = require('./routes/idsRoutes');
 
 app.use('/api', idsRoutes);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 7878;
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
