68c112d4479a1d3ce3659197f431a35c7e4d5f4d
Vineet <vineetsah1407@gmail.com>
2025-04-30 19:26:12 +0530

    docker integration updated

--- server/server.js
+++ server/server.js
@@ -15,8 +15,9 @@ const PORT = 4000;
 app.use(cors());
 app.use(express.json());
 
-// ─── MONGODB SETUP ────────────────────────────────────────────────────────────
-const mongoURI = 'mongodb://localhost:27017/intrusai_db';
+// ─── MONGODB SETUP ─────────
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/IntrusAI"
+const mongoURI = `${MONGO_URI}`;
 mongoose.connect(mongoURI, {
   useNewUrlParser:    true,
   useUnifiedTopology: true
@@ -59,8 +60,9 @@ app.post('/upload', upload.single('file'), async (req, res) => {
     }));
     console.log('🔍 Parsed & tweaked JSON length:', rows.length);
 
+    const FLASK_URL = process.env.FLASK_URL || "http://127.0.0.1:5000";
     // 3) forward to Flask ML service
-    const mlRes = await axios.post('http://127.0.0.1:5000/predict', rows);
+    const mlRes = await axios.post(`${FLASK_URL}/predict`, rows);
     console.log('✅ ML API responded:', mlRes.data);
 
     // 4) read file back into a Buffer
